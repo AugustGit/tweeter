acc31c267e0174082dce82abd4f94e0ff95cffff
austin metka <austin_metka@hotmail.com>
2018-01-31 00:00:11 +0000

    new tweet box made, character counter functional, tweet display in process

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$( document ).ready(function() {
+ $.fn.log = function() {
+    console.log.apply(console, this);
+    return this;
+  };
+});
---
+++ public/scripts/counter.js
@@ -0,0 +1,28 @@
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    //console.log( "ready!" );
+   var charRemaining = 140
+   $('#tweet-area').on('keypress', function() {
+    //console.log('#tweet-area')
+     var tweetLength = $('#tweet-area').val().length;
+     charRemaining  = 139 - tweetLength
+
+     if(charRemaining >= 0){
+       $(this).siblings(".tweet-char-counter").html(charRemaining)
+      } else {
+       $(this).siblings(".tweet-char-counter").html(charRemaining)
+      .css("color", "red");
+    }
+   });
+});
+
+/*
+var input = document.getElementById('myInput');
+
+input.onkeydown = function() {
+    var key = event.keyCode || event.charCode;
+
+    if( key == 8 || key == 46 )
+        return false;
+};*/
